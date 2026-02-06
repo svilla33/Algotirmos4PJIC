c304c6cb4f136ee794aebb1baf3dabbd0549c976
svilla33 <santiagovilla333@gmail.com>
2026-02-05 19:41:04 -0500

    borrando archivos duplicados

--- estudiante.py
+++
@@ -1,8 +0,0 @@
-class Estudiante:
-    def __init__(self, nombre, documento):
-        self.nombre = nombre
-        self.documento = documento
-
-estudiente1 = Estudiante("Juan", 111)
-
-print(estudiente1.nombre,estudiente1.documento)
