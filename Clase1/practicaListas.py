e98df3e75da606b7ae65d819e6143cb4233ad93d
svilla33 <santiagovilla333@gmail.com>
2026-02-05 20:27:26 -0500

    Pequeno repaso de como hacer una lista ligada simple

---
+++ Clase1/practicaListas.py
@@ -0,0 +1,41 @@
+class Nodo:
+    def __init__(self, documento, nombre):#supongamos que son pacientes de un hospital
+        self.documento = documento
+        self.nombre = nombre
+        self.siguiente = None
+        self.cola = None
+
+class Lista:
+    def __init__(self):
+        self.cabeza = None
+
+    def AgregarAlFinal(self, documento, nombre):
+        nodo = Nodo(documento,nombre)
+
+        if self.cabeza == None:
+            self.cabeza =nodo
+            self.cola = nodo
+        else:
+            self.cola.siguiente = nodo
+            self.cola = nodo
+
+    #else mal optimizado, tiene que recorrer todo el nodo
+        ''' 
+        else:
+            actual = self.cabeza
+            while actual.siguiente != None: #tambien puede ser simplemente "actual.siguiente:"
+                actual = actual.siguiente
+            actual.siguiente = nodo
+        '''
+            
+
+    
+        
+
+
+
+
+
+
+
+
